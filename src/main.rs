268520e68553bec92625bee4f50f69d633dc16bb
MaxTheMighty <maxwellkozlov1@gmail.com>
2022-12-28 18:03:56 -0800

    Added button, using updates and messages

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@
 
 use iced::*;
 
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    AddChar
+}
 struct BasicStruct{
     my_text: String
 }
 
 impl Sandbox for BasicStruct{
-    type Message = ();
+    type Message = Message;
     //Constructor (sorta)
     fn new() -> BasicStruct {
         BasicStruct {my_text: String::from("Default String")}
@@ -24,12 +28,21 @@ impl Sandbox for BasicStruct{
     //this is where the logic of our program is called. We use update to modify the state of our program using Messages
     fn update(&mut self, message: Self::Message){
         println!("Update called");
-        self.my_text.push('a')
+        match(message){
+            Message::AddChar =>{
+                self.my_text.push('a')
+            }
+        }
+       
     }
 
     //returns widgets that will be displayed on our program
     fn view(&self) -> Element<Self::Message>{
-       iced::widget::text(&self.my_text).into()
+       iced::widget::row![
+        iced::widget::text(&self.my_text),
+        iced::widget::button("Add char").on_press(Message::AddChar)
+       ].padding(20).into()
+       
     }
 }
 
