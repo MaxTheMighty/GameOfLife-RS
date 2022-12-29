1fd57f249ce7be60e897cb3a0b17f25ca7d4490c
MaxTheMighty <maxwellkozlov1@gmail.com>
2022-12-28 17:57:17 -0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+//TODO  Learn how canvas works in iced
+// - Make a basic running GUI program
+// - Successfully make a Grid that implements program
+// - Use this GridProgram implementation to make a running application
+
+use iced::*;
+
+struct BasicStruct{
+    my_text: String
+}
+
+impl Sandbox for BasicStruct{
+    type Message = ();
+    //Constructor (sorta)
+    fn new() -> BasicStruct {
+        BasicStruct {my_text: String::from("Default String")}
+    }
+
+    //Returns the title of our program
+    fn title(&self) -> String{
+        String::from("1")
+    }
+
+    //this is where the logic of our program is called. We use update to modify the state of our program using Messages
+    fn update(&mut self, message: Self::Message){
+        println!("Update called");
+        self.my_text.push('a')
+    }
+
+    //returns widgets that will be displayed on our program
+    fn view(&self) -> Element<Self::Message>{
+       iced::widget::text(&self.my_text).into()
+    }
+}
+
+fn main() -> iced::Result{
+    <BasicStruct as Sandbox>::run(iced::Settings::default())
+}
